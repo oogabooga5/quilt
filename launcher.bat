b7a3fd20265c305442933e27df6637a5139cdb62
oogabooga5 <bmcisaa4324@sudburycdsb.ca>
2023-11-11 15:32:43 -0500

    fix windows launcher

---
+++ launcher.bat
@@ -0,0 +1,8 @@
+@echo off
+pip install pyautogui
+pip install colorify
+set /p ver="Quilt version: "
+set "url=https://raw.githubusercontent.com/oogabooga5/quilt/main/%ver%/quilt.py"
+curl %url% -O quilt.py
+python quilt.py
+del quilt.py
